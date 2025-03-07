816e1e2551d5f7d668a58a2f5faa830087109495
Nikita Gorynin <nikita12341@mail.ru>
2025-03-07 14:45:04 +0300

    upd allowed host with task-manager-ulqx.onrender.com

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -31,7 +31,12 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['webserver', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = [
+    'webserver',
+    '127.0.0.1',
+    'localhost',
+    'task-manager-ulqx.onrender.com',
+    ]
 
 
 # Application definition
