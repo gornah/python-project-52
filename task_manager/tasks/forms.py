b93e4c983dd93bcded734ab0ddae579e558b218f
Nikita Gorynin <nikita12341@mail.ru>
2025-03-31 15:41:37 +0300

    udp task form

--- task_manager/tasks/forms.py
+++ task_manager/tasks/forms.py
@@ -6,73 +6,62 @@ from task_manager.users.models import User
 from task_manager.labels.models import Label
 
 
-class TaskForm(forms.ModelForm):
-    name = forms.CharField(
-        label="Имя",
-        label_suffix='',
-        widget=forms.TextInput(
-            attrs={
-                'class': 'form-control',
-                'placeholder': 'Имя',
-            }
-        )
-    )
-
-    description = forms.CharField(
-        label="Описание",
-        label_suffix='',
-        widget=forms.Textarea(
-            attrs={
-                'class': 'form-control',
-                'placeholder': 'Описание',
-            }
-        )
-    )
-
-    status = forms.ModelChoiceField(
-        queryset=Status.objects.all(),
-        label="Статус",
-        label_suffix='',
-        widget=forms.Select(
-            attrs={
-                'class': 'form-control',
-            }
-        )
-    )
-
-    executor = forms.ModelChoiceField(
-        queryset=User.objects.all(),
-        label="Исполнитель",
-        label_suffix='',
-        widget=forms.Select(
-            attrs={
-                'class': 'form-control',
-            }
-        )
-    )
-
-    labels = forms.ModelMultipleChoiceField(
-        queryset=Label.objects.all(),
-        label="Метки",
-        label_suffix='',
-        widget=forms.SelectMultiple(
-            attrs={
-                'class': 'form-control',
-            }
-        )
-    )
-
-    class Meta:
-        model = Task
-        fields = (
-            'name',
-            'description',
-            'status',
-            'executor',
-            'labels'
-        )
-
 # class TaskForm(forms.ModelForm):
+#     name = forms.CharField(
+#         label="Имя",
+#         label_suffix='',
+#         widget=forms.TextInput(
+#             attrs={
+#                 'class': 'form-control',
+#                 'placeholder': 'Имя',
+#             }
+#         )
+#     )
+
+#     description = forms.CharField(
+#         label="Описание",
+#         label_suffix='',
+#         widget=forms.Textarea(
+#             attrs={
+#                 'class': 'form-control',
+#                 'placeholder': 'Описание',
+#             }
+#         )
+#     )
+
+#     status = forms.ModelChoiceField(
+#         queryset=Status.objects.all(),
+#         label="Статус",
+#         label_suffix='',
+#         widget=forms.Select(
+#             attrs={
+#                 'class': 'form-control',
+#             }
+#         )
+#     )
+
+#     executor = forms.ModelChoiceField(
+#         queryset=User.objects.all(),
+#         label="Исполнитель",
+#         label_suffix='',
+#         widget=forms.Select(
+#             attrs={
+#                 'class': 'form-control',
+#             }
+#         )
+#     )
+
+#     labels = forms.ModelMultipleChoiceField(
+#         queryset=Label.objects.all(),
+#         label="Метки",
+#         label_suffix='',
+#         widget=forms.SelectMultiple(
+#             attrs={
+#                 'class': 'form-control',
+#             }
+#         )
+#     )
+
 #     class Meta:
 #         model = Task
 #         fields = (
@@ -82,42 +71,53 @@ class TaskForm(forms.ModelForm):
 #             'executor',
 #             'labels'
 #         )
-#         widgets = {
-#             'name': forms.TextInput(
-#                 attrs={
-#                     'class': 'form-control',
-#                     'placeholder': 'Имя',
-#                 }
-#             ),
-#             'description': forms.Textarea(
-#                 attrs={
-#                     'class': 'form-control',
-#                     'placeholder': 'Описание',
-#                 }
-#             ),
-#             'status': forms.Select(
-#                 attrs={
-#                     'class': 'form-control',
-#                     'choices': Status,
-#                 }
-#             ),
-#             'executor': forms.Select(
-#                 attrs={
-#                     'class': 'form-control',
-#                     'choices': User,
-#                 }
-#             ),
-#             'labels': forms.SelectMultiple(
-#                 attrs={
-#                     'class': 'form-control',
-#                     'choices': Label,
-#                 }
-#             ),
-#         }
-#         labels = {
-#             'name': 'Имя',
-#             'description': 'Описание',
-#             'status': 'Статус',
-#             'executor': 'Исполнитель',
-#             'labels': 'Метки'
-#         }
+
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = (
+            'name',
+            'description',
+            'status',
+            'executor',
+            'labels'
+        )
+        widgets = {
+            'name': forms.TextInput(
+                attrs={
+                    'class': 'form-control',
+                    'placeholder': 'Имя',
+                }
+            ),
+            'description': forms.Textarea(
+                attrs={
+                    'class': 'form-control',
+                    'placeholder': 'Описание',
+                }
+            ),
+            'status': forms.Select(
+                attrs={
+                    'class': 'form-control',
+                    'choices': Status,
+                }
+            ),
+            'executor': forms.Select(
+                attrs={
+                    'class': 'form-control',
+                    'choices': User,
+                }
+            ),
+            'labels': forms.SelectMultiple(
+                attrs={
+                    'class': 'form-control',
+                    'choices': Label,
+                }
+            ),
+        }
+        labels = {
+            'name': 'Имя',
+            'description': 'Описание',
+            'status': 'Статус',
+            'executor': 'Исполнитель',
+            'labels': 'Метки'
+        }
