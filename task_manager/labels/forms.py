b469fefe815f8306fbc22762d49dcbd78199f49d
Nikita Gorynin <nikita12341@mail.ru>
2025-03-31 14:56:46 +0300

    upd task, status, label forms

--- task_manager/labels/forms.py
+++ task_manager/labels/forms.py
@@ -1,21 +1,17 @@
 from django import forms
-
 from .models import Label
 
 
 class LabelForm(forms.ModelForm):
+    name = forms.CharField(
+        label="Имя",
+        label_suffix='',
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Имя'
+            }),
+        )
 
     class Meta:
         model = Label
         fields = ['name']
-        widgets = {
-            'name': forms.TextInput(
-                attrs={
-                    'class': 'form-control',
-                    'placeholder': 'Имя'
-                }
-            ),
-        }
-        labels = {
-            'name': 'Имя',
-        }
--- task_manager/statuses/forms.py
+++ task_manager/statuses/forms.py
@@ -1,20 +1,17 @@
 from django import forms
-from task_manager.statuses.models import Status
+from .models import Status
 
 
 class StatusForm(forms.ModelForm):
+    name = forms.CharField(
+        label="Имя",
+        label_suffix='',
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Имя'
+            }),
+        )
 
     class Meta:
         model = Status
-        fields = 'name',
-        widgets = {
-            'name': forms.TextInput(
-                attrs={
-                    'class': 'form-control',
-                    'placeholder': 'Имя'
-                }
-            ),
-        }
-        labels = {
-            'name': 'Имя',
-        }
+        fields = ['name']
